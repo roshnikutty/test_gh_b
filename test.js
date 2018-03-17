c00080bbb9c9bab629f5d6593dc72036196161cb
roshnikutty <roshni.ajkutty@gmail.com>
2018-03-17 10:48:31 -0400

    corrected typo in devel-feature1

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function testing() {
     console.log("Main- first commit"); 
     console.log("adding stuff to devel branch");
-    console.log("adding stuff to devel-feature1 brach");
+    console.log("adding stuff to devel-feature1 branch");
 }
 
 function testing();
