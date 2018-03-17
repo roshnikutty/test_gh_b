855e55bb66cca0a71b87fe47beb16baa7d07d377
roshnikutty <roshni.ajkutty@gmail.com>
2018-03-17 10:51:45 -0400

    added text for devel-feature2 test

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 function testing() {
     console.log("Main- first commit"); 
     console.log("adding stuff to devel branch");
+    console.log("adding stuff to devel-feature 2"); 
 }
 
 function testing();
