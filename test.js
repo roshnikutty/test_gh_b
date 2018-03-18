dbdbdf74fd3b40366f8cdafb278ed3a8c743127c
Roshni Kutty <roshni.ajkutty@gmail.com>
2018-03-18 10:13:58 -0400

    update to branchfeature2 from another machine

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function testing() {
     console.log("Main- first commit"); 
     console.log("adding stuff to devel branch");
-    console.log("adding stuff to devel-feature 2"); 
+    console.log("adding stuff to devel-feature 2- updated on another machine"); 
 }
 
 function testing();
